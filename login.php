d75ee307e1dc5e08bb5289faaa983728697b9acb
ImperatorDeos <njmm98@hotmail.com>
2018-09-30 19:16:59 +0800

    enabled basic authentication with successful login taking user to a home page. user is also able to logout. trying to access page without logging in results in redirect to login page.

---
+++ login.php
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xml:lang="en">
+  <head>
+     <title>Login</title>
+     <link rel="stylesheet" href="style.css" title="Style Sheet" type="text/css" />
+     <?php
+      include "connection.php";
+     ?>
+  </head>
+
+  <body>
+
+      <form action="login.php" method="POST">
+        <label for="user">Username:</label> <input type="text" name="user" id="user">
+        <label for="pass">Password:</label> <input type="password" name="pass" id="pass">
+        <input type="submit" id="login" value="Login" name="submit">
+      </form>
+
+    <?php
+     if (isset($_POST['submit'])){
+    	session_start();
+
+    	$username = $_POST['user'];
+    	$password = $_POST['pass'];
+
+
+      $query = "SELECT * FROM member WHERE username = '$username'";
+
+    	$result = pg_query($db,$query);
+      $userdata = pg_fetch_array($result);
+
+    /*  if(pg_fetch_row($result) == 0){
+	        echo "Please try again";
+      }*/
+      if($password != $userdata['password']){
+        echo "Please try again";
+      }else{
+		      session_regenerate_id();
+		      $_SESSION['fname'] = $userdata['fname'];
+          $_SESSION['lname'] = $userdata['lname'];
+		      $_SESSION['username'] = $userdata['username'];
+		      session_write_close();
+		      header('location: home.php');
+	     }
+
+
+    }
+
+
+
+    ?>
+
+
+
+
+  </body>
+  </html>
---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+
+// include connection to database
+include "connection.php";
+
+
+//end session
+session_start();
+session_destroy();
+
+//redirect to login
+header ('Location: login.php');
+
+?>
